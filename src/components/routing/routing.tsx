a2b3b762385e659598889688414aa83da4e511a0
AKUU <57586401+Akuu29@users.noreply.github.com>
2024-05-06 19:36:16 +0900

    Add: Added routing

---
+++ src/components/routing/routing.tsx
@@ -0,0 +1,28 @@
+import { createBrowserRouter } from "react-router-dom";
+import Default from "../layout/Default";
+import ErrorPage from "../layout/ErrorPage";
+import Articles from "../../views/Articles";
+import Article from "../../views/Article";
+import HelloWorld from "../../views/HelloWorld";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Default />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        "path": "/hello",
+        element: <HelloWorld />,
+      },
+      {
+        "path": "/",
+        element: <Articles />
+      },
+      {
+        "path": "/article/:id",
+        element: <Article />
+      }
+    ]
+  }
+]);
