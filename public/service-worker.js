38d7b0e8635ab05b2f51c2b16fe002b1520dd528
AKUU <57586401+Akuu29@users.noreply.github.com>
2024-09-29 21:53:18 +0900

    Add: Added service worker

---
+++ public/service-worker.js
@@ -0,0 +1,42 @@
+let apiBaseUrl = "";
+let accessToken = null;
+
+self.addEventListener("message", (event) => {
+  const { type, message } = event.data
+
+  switch (type) {
+    case "SET_API_BASE_URL":
+      apiBaseUrl = message;
+      break;
+    case "SET_ACCESS_TOKEN":
+      accessToken = message;
+      break;
+    default:
+      break;
+  }
+
+});
+
+self.addEventListener("fetch", (event) => {
+  const request = event.request;
+
+  if (request.url.startsWith(apiBaseUrl)) {
+    event.respondWith(
+      (async () => {
+        if (accessToken) {
+          const modifiedRequest = new Request(request, {
+            headers: new Headers({
+              ...Object.fromEntries(request.headers.entries()),
+              Authorization: `Bearer ${accessToken}`
+            })
+          });
+
+          console.log("Modified request", modifiedRequest);
+          return fetch(modifiedRequest);
+        } else {
+          return fetch(request);
+        }
+      })()
+    );
+  }
+});
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js")
+      .then((registration) => {
+        console.log("ServiceWorker registration successful with scope: ", registration.scope);
+
+        if (registration.active) {
+          registration.active.postMessage({
+            type: "SET_API_BASE_URL",
+            message: API_BASE_URL
+          });
+        } else {
+          navigator.serviceWorker.addEventListener("controllerchange", () => {
+            if (navigator.serviceWorker.controller) {
+              navigator.serviceWorker.controller.postMessage({
+                type: "SET_API_BASE_URL",
+                message: API_BASE_URL
+              });
+            }
+          });
+        }
+      })
+      .catch((error) => {
+        console.log("ServiceWorker registration failed: ", error);
+      });
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
