d9a85b4f32be2114ee88f87e321f87a0c31184bd
AKUU <57586401+Akuu29@users.noreply.github.com>
2024-11-10 23:44:57 +0900

    Add: Added type related Category

---
+++ src/types/category.d.ts
@@ -0,0 +1,14 @@
+export type Category = {
+  id: number;
+  name: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type NewCategory = {
+  name: string;
+};
+
+export type UpdateCategory = {
+  name: string;
+};
