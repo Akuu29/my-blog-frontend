749f0695d99e26656c86d726acd2dc90d265f8f2
AKUU <57586401+Akuu29@users.noreply.github.com>
2024-11-11 07:48:00 +0900

    Add: Added class "CategoryApi"
    * CategoryAPI handls external WebAPI related to Category

---
+++ src/services/category_api.ts
@@ -0,0 +1,104 @@
+import axios, { AxiosError } from "axios";
+import { Result } from "../utils/result";
+import type { ErrorResponse, ErrorStatusCodes } from "../types/error_response";
+import type { Category, NewCategory, UpdateCategory } from "../types/category";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export class CategoryApi {
+  static async all(): Promise<Result<Array<Category>, ErrorResponse>> {
+    try {
+      const response = await axios.get(`${API_BASE_URL}/categories`);
+
+      return Result.ok(response.data);
+    } catch (err) {
+      if (err instanceof AxiosError) {
+        const errorResponse = {
+          message: err.message,
+          status: Number(err.status) as ErrorStatusCodes
+        };
+
+        return Result.err(errorResponse);
+      }
+
+      const errorResponse = {
+        message: "Unknown error occurred",
+        status: 500 as ErrorStatusCodes
+      };
+
+      return Result.err(errorResponse);
+    }
+  }
+
+  static async create(category: NewCategory): Promise<Result<Category, ErrorResponse>> {
+    try {
+      const response = await axios.post(`${API_BASE_URL}/categories`, category);
+
+      return Result.ok(response.data);
+    } catch (err) {
+      if (err instanceof AxiosError) {
+        const errorResponse = {
+          message: err.message,
+          status: Number(err.status) as ErrorStatusCodes
+        };
+
+        return Result.err(errorResponse);
+      }
+
+      const errorResponse = {
+        message: "Unknown error occurred",
+        status: 500 as ErrorStatusCodes
+      };
+
+      return Result.err(errorResponse);
+    }
+  }
+
+  static async update(category_id: number, category: UpdateCategory): Promise<Result<Category, ErrorResponse>> {
+    try {
+      const response = await axios.patch(`${API_BASE_URL}/categories/${category_id}`, category);
+
+      return Result.ok(response.data);
+    } catch (err) {
+      if (err instanceof AxiosError) {
+        const errorResponse = {
+          message: err.message,
+          status: Number(err.status) as ErrorStatusCodes
+        };
+
+        return Result.err(errorResponse);
+      }
+
+      const errorResponse = {
+        message: "Unknown error occurred",
+        status: 500 as ErrorStatusCodes
+      };
+
+      return Result.err(errorResponse);
+    }
+  }
+
+  static async delete(category_id: number): Promise<Result<null, ErrorResponse>> {
+    try {
+      await axios.delete(`${API_BASE_URL}/categories/${category_id}`);
+
+      return Result.ok(null);
+    } catch (err) {
+      if (err instanceof AxiosError) {
+        const errorResponse = {
+          message: err.message,
+          status: Number(err.status) as ErrorStatusCodes
+        };
+
+        return Result.err(errorResponse);
+      }
+
+      const errorResponse = {
+        message: "Unknown error occurred",
+        status: 500 as ErrorStatusCodes
+      };
+
+      return Result.err(errorResponse);
+    }
+  }
+}
