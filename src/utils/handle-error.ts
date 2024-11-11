6bcb5cbaa3e07944ee58a850b76085b235a43c75
AKUU <57586401+Akuu29@users.noreply.github.com>
2024-11-11 12:34:19 +0900

    Add: Added handling error from webapi

---
+++ src/utils/handle-error.ts
@@ -0,0 +1,21 @@
+import { useNavigate } from 'react-router-dom';
+import { match } from "ts-pattern";
+import type { ErrorResponse } from "../types/error_response";
+
+function handleError(
+  error: ErrorResponse,
+  navigator: ReturnType<typeof useNavigate>,
+  openSnackbar: (vertical: "top" | "bottom", horizontal: "left" | "center" | "right", message: string) => void,
+  vertical: "top" | "bottom",
+  horizontal: "left" | "center" | "right",
+) {
+  match(error.status)
+    .with(400, () => openSnackbar(vertical, horizontal, error.message))
+    .with(401, () => navigator("/signin"))
+    .with(403, () => openSnackbar(vertical, horizontal, error.message))
+    .with(404, () => openSnackbar(vertical, horizontal, error.message))
+    .with(500, () => openSnackbar(vertical, horizontal, error.message))
+    .exhaustive();
+}
+
+export default handleError;
