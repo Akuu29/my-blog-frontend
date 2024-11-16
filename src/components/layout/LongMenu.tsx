df51df3650fecfa92855a164f352633f624a5f4c
AKUU <57586401+Akuu29@users.noreply.github.com>
2024-11-17 00:51:13 +0900

    Add: Added LognMenu

---
+++ src/components/layout/LongMenu.tsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+
+const ITEM_HEIGHT = 48;
+
+function LongMenu({ options, clickMenuItemHandler }: { options: Array<string>, clickMenuItemHandler: (option: string) => void }) {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const handleClickMenuItem = (event: React.MouseEvent<HTMLElement>) => {
+    const target = event.target as HTMLElement;
+    handleClose();
+    clickMenuItemHandler(target.textContent!);
+  };
+
+  return (
+    <div>
+      <IconButton
+        aria-label="more"
+        id="long-button"
+        aria-controls={open ? 'long-menu' : undefined}
+        aria-expanded={open ? 'true' : undefined}
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        <MoreVertIcon />
+      </IconButton>
+      <Menu
+        id="long-menu"
+        MenuListProps={{
+          'aria-labelledby': 'long-button',
+        }}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        disableEnforceFocus
+        disableRestoreFocus
+        slotProps={{
+          paper: {
+            style: {
+              maxHeight: ITEM_HEIGHT * 4.5,
+              width: '20ch',
+            },
+          },
+        }}
+      >
+        {options.map((option) => (
+          <MenuItem
+            key={option}
+            onClick={handleClickMenuItem}
+            sx={{ fontFamily: 'monospace' }}
+          >
+            {option}
+          </MenuItem>
+        ))}
+      </Menu>
+    </div>
+  );
+}
+
+export default LongMenu;
