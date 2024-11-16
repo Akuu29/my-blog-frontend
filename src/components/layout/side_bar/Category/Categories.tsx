5b2385c63661b1a11930ef50a6150bbb82feec63
AKUU <57586401+Akuu29@users.noreply.github.com>
2024-11-17 00:52:32 +0900

    Add: Added Categories

---
+++ src/components/layout/side_bar/Category/Categories.tsx
@@ -0,0 +1,181 @@
+import { useState, useEffect, useContext } from "react";
+import { useNavigate } from 'react-router-dom';
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import IconButton from "@mui/material/IconButton";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Pagination from "@mui/material/Pagination";
+import type { Category } from "../../../../types/category";
+import { CategoryApi } from "../../../../services/category_api";
+import CategoryRow from "./CategoryRow";
+import { UserStatusContext } from "../../../../contexts/UserStatusContext";
+import type { UserStatusContextProps } from "../../../../types/user_status_context";
+import { ErrorSnackbarContext } from "../../../../contexts/ErrorSnackbarContext";
+import type { ErrorSnackbarContextProps } from "../../../../types/error_snackbar_context";
+import handleError from "../../../../utils/handle-error";
+import { Result } from "../../../../utils/result";
+import type { ErrorResponse } from "../../../../types/error_response";
+
+const CATEGORY_PER_PAGE = 5;
+
+function Categories() {
+  const navigate = useNavigate();
+  const userStatus = useContext(UserStatusContext) as UserStatusContextProps;
+  const { openSnackbar } = useContext(ErrorSnackbarContext) as ErrorSnackbarContextProps;
+
+  const [categories, setCategories] = useState<Array<Category>>([]);
+  useEffect(() => {
+    (async () => {
+      const result = await CategoryApi.all();
+
+      if (result.isOk()) {
+        setCategories(result.unwrap());
+      }
+
+      handleError((result.unwrap() as ErrorResponse), navigate, openSnackbar, "top", "right");
+
+    })();
+
+  }, [navigate, openSnackbar]);
+
+  const deleteCategory = async (category: Category) => {
+    const result = await CategoryApi.delete(category.id);
+
+    if (result.isOk()) {
+      setCategories(categories.filter((c) => c.id !== category.id));
+    }
+
+    handleError(result.unwrap() as ErrorResponse, navigate, openSnackbar, "top", "right");
+  };
+
+  const updateCategory = async (category: Category, editCategoryName: string): Promise<Result<null, null>> => {
+    const result = await CategoryApi.update(category.id, { name: editCategoryName });
+
+    if (result.isOk()) {
+      setCategories(categories.map((c) => c.id === category.id ? result.unwrap() : c));
+
+      return Result.ok(null);
+    }
+
+    handleError(result.unwrap() as ErrorResponse, navigate, openSnackbar, "top", "right");
+
+    return Result.err(null);
+  };
+
+  const [addCategory, setAddCategory] = useState(false);
+  const handleClickAddIcon = () => {
+    setAddCategory(true);
+  };
+  const handleClickRemoveIcon = () => {
+    setAddCategory(false);
+  };
+
+  const [page, setPage] = useState(1);
+  const handleChangePage = (_event: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+  };
+
+  const startIndex = (page - 1) * CATEGORY_PER_PAGE;
+  const endIndex = startIndex + CATEGORY_PER_PAGE;
+  const paginatedCategories = categories.slice(startIndex, endIndex);
+
+  const [newCategory, setNewCategory] = useState<string>("");
+
+  const handleChangeCategory = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewCategory(event.target.value);
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const category = data.get("category");
+
+    if (category) {
+      const result = await CategoryApi.create({ name: category as string });
+
+      if (result.isOk()) {
+        setCategories([...categories, result.unwrap()]);
+        setNewCategory("");
+      }
+
+      handleError(result.unwrap() as ErrorResponse, navigate, openSnackbar, "top", "right");
+    }
+  };
+
+  return (
+    <>
+      <Box sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        p: 1,
+      }}>
+        <Typography sx={{
+          fontFamily: 'monospace',
+          variant: "h1",
+          fontWeight: 600,
+        }}>
+          Category
+        </Typography>
+        {userStatus.isLoggedIn && !addCategory && (
+          <IconButton onClick={handleClickAddIcon}>
+            <AddIcon />
+          </IconButton>
+        )}
+        {userStatus.isLoggedIn && addCategory && (
+          <IconButton onClick={handleClickRemoveIcon}>
+            <RemoveIcon />
+          </IconButton>
+        )}
+      </Box>
+      {userStatus.isLoggedIn && addCategory && (
+        <Box
+          component={"form"}
+          onSubmit={handleSubmit}
+          sx={{
+            p: 1,
+            display: "flex",
+            alignItems: "flex-end",
+          }}
+        >
+          <TextField
+            label="Add category"
+            variant="standard"
+            name="category"
+            value={newCategory}
+            onChange={handleChangeCategory}
+            sx={{ flexGrow: 1, mr: 1, }}
+          />
+          <Button
+            variant="outlined"
+            type="submit"
+          >
+            Add
+          </Button>
+        </Box>
+      )}
+      {paginatedCategories.map((category) => (
+        <CategoryRow
+          category={category}
+          userStatus={userStatus}
+          updateCategoryHandler={updateCategory}
+          deleteCategoryHandler={deleteCategory}
+        />
+      ))}
+      {categories.length > 0 && (
+        <Box sx={{
+          display: "flex",
+          justifyContent: "center",
+          mt: 2,
+        }} >
+          <Pagination count={Math.ceil(categories.length / CATEGORY_PER_PAGE)} size="small" page={page} onChange={handleChangePage} />
+        </Box>
+      )}
+    </>
+  );
+}
+
+export default Categories;
---
+++ src/components/layout/side_bar/Category/CategoryRow.tsx
@@ -0,0 +1,115 @@
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import { UserStatusContextProps } from "../../../../types/user_status_context";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import LongMenu from "../../LongMenu";
+import type { Category } from "../../../../types/category";
+import { Result } from "../../../../utils/result";
+
+const OPTIONS = [
+  "Edit",
+  "Delete",
+];
+
+const DELETE_CONFIRM_MESSAGE = `Articles in this category will be moved to "None".
+Are you sure you want to delete it?`;
+
+function CategoryRow({
+  category,
+  userStatus,
+  updateCategoryHandler,
+  deleteCategoryHandler,
+}: {
+  category: Category,
+  userStatus: UserStatusContextProps,
+  updateCategoryHandler: (category: Category, editCategoryName: string) => Promise<Result<null, null>>,
+  deleteCategoryHandler: (category: Category) => void,
+}) {
+  const navigate = useNavigate();
+  const [isEdit, setIsEdit] = useState(false);
+  const [editCategoryName, setEditCategoryName] = useState(category.name);
+
+  const handleClickCategory = () => {
+    navigate(`/category/${category.id}`);
+  };
+
+  const handleChangeEditCategoryName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEditCategoryName(event.target.value);
+  };
+
+  const handleBlurEditCategoryName = async () => {
+    const result = await updateCategoryHandler(category, editCategoryName);
+    if (result.isErr()) setEditCategoryName(category.name);
+    setIsEdit(false);
+  };
+
+  const handelKeyDownEditCategoryName = async (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      const result = await updateCategoryHandler(category, editCategoryName);
+      if (result.isErr()) setEditCategoryName(category.name);
+      setIsEdit(false);
+    }
+  };
+
+  const deleteCategory = () => {
+    if (window.confirm(DELETE_CONFIRM_MESSAGE)) {
+      deleteCategoryHandler(category);
+    }
+  };
+
+  const handleClickMenuItem = (option: string) => {
+    switch (option) {
+      case "Edit":
+        setIsEdit(true);
+        break;
+      case "Delete":
+        deleteCategory();
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+      }}>
+      <Box sx={{ flexGrow: 1 }}>
+        {isEdit ? (
+          <TextField
+            variant="standard"
+            value={editCategoryName}
+            onChange={handleChangeEditCategoryName}
+            onBlur={handleBlurEditCategoryName}
+            onKeyDown={handelKeyDownEditCategoryName}
+            autoFocus
+          />
+        ) : (
+          <Link
+            underline="hover"
+            sx={{ cursor: "pointer" }}
+            onClick={handleClickCategory}>
+            <Typography
+              sx={{
+                m: 1,
+                fontFamily: 'monospace',
+              }}
+              key={category.id}>
+              {category.name}
+            </Typography>
+          </Link>
+        )}
+      </Box>
+      {userStatus.isLoggedIn && (
+        <LongMenu options={OPTIONS} clickMenuItemHandler={handleClickMenuItem} />
+      )}
+    </Box>
+  );
+}
+
+export default CategoryRow;
