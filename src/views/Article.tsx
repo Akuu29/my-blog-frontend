a965a445854e32fd5565811df544cd628e759212
AKUU <57586401+Akuu29@users.noreply.github.com>
2024-12-14 23:07:15 +0900

    Add: Added Error handling to components related article

--- src/views/Article.tsx
+++ src/views/Article.tsx
@@ -1,19 +1,32 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import { Grid, Typography } from "@mui/material";
 
 import ArticleApi from "../services/article-api";
+import handleError from "../utils/handle-error";
+import { ErrorSnackbarContext } from "../contexts/ErrorSnackbarContext";
 import type { Article } from "../types/article";
+import type { ErrorSnackbarContextProps } from "../types/error-snackbar-context";
 
 function Article() {
-  const { id } = useParams<{ id: string }>();
+  const navigate = useNavigate();
+  const { openSnackbar } = useContext(ErrorSnackbarContext) as ErrorSnackbarContextProps;
+  const { article_id } = useParams<{ article_id: string }>();
   const [article, setArticle] = useState<Article>();
 
   useEffect(() => {
-    ArticleApiLocal.getArticle(Number(id)).then((article) => {
-      setArticle(article);
-    })
-  }, [id]);
+    (async () => {
+      if (article_id) {
+        const result = await ArticleApi.find(Number(article_id));
+
+        if (result.isOk()) {
+          setArticle(result.unwrap());
+        } else if (result.isErr()) {
+          handleError(result.unwrap(), navigate, openSnackbar, "top", "center");
+        }
+      }
+    })();
+  }, [article_id, navigate, openSnackbar]);
 
   return (
     <Grid container spacing={2} alignItems={"flex-end"}>
--- src/views/Articles.tsx
+++ src/views/Articles.tsx
@@ -1,20 +1,32 @@
+import { useContext } from "react";
 import { Grid, Stack, Typography } from "@mui/material";
 import { Card } from "@mui/material";
 import { useState, useEffect } from "react";
 
 import ArticleApi from "../services/article-api";
 import { useNavigate } from "react-router-dom";
+import handleError from "../utils/handle-error";
+import { ErrorSnackbarContext } from "../contexts/ErrorSnackbarContext";
 import type { Article } from "../types/article";
+import type { ErrorSnackbarContextProps } from "../types/error-snackbar-context";
+
 
 function Articles() {
   const navigate = useNavigate();
+  const { openSnackbar } = useContext(ErrorSnackbarContext) as ErrorSnackbarContextProps;
   const [articles, setArticles] = useState<Array<Article>>([]);
 
   useEffect(() => {
-    ArticleApiLocal.getArticles().then((articles) => {
-      setArticles(articles);
-    });
-  }, []);
+    (async () => {
+      const result = await ArticleApi.all();
+
+      if (result.isOk()) {
+        setArticles(result.unwrap());
+      } else if (result.isErr()) {
+        handleError(result.unwrap(), navigate, openSnackbar, "top", "center");
+      }
+    })();
+  }, [navigate, openSnackbar]);
 
   const onClickArticle = (article: Article) => {
     navigate(`/article/${article.id}`);
