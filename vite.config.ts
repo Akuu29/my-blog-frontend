8c1c8593281c88b3596098a5842f5e46e2955e0e
AKUU <57586401+Akuu29@users.noreply.github.com>
2025-02-01 18:50:12 +0900

    Add: Added https settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+// import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Uncomment the following lines to enable HTTPS
+  // server: {
+  //   https: {
+  //     key: fs.readFileSync("./certs/localhost-key.pem"),
+  //     cert: fs.readFileSync("./certs/localhost.pem"),
+  //   }
+  // }
 });
