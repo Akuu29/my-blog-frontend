cab481cf920016e47c07dbb3e589ee7cd64f1fcb
AKUU <57586401+Akuu29@users.noreply.github.com>
2025-07-12 23:36:54 +0900

    Add: Added ci actions

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
+import { cloudflare } from '@cloudflare/vite-plugin';
 import react from '@vitejs/plugin-react-swc';
 // import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), cloudflare()],
   // Uncomment the following lines to enable HTTPS
   // server: {
   //   https: {
