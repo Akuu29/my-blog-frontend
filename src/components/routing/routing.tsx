580290eb6804063aa06b07ab134973ebf0c7a35e
AKUU <57586401+Akuu29@users.noreply.github.com>
2025-08-12 02:28:59 +0900

    Add: Added top page

--- src/components/routing/routing.tsx
+++ src/components/routing/routing.tsx
@@ -1,12 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import BasePage from "../layout/BasePage";
 import ErrorPage from "../layout/ErrorPage";
+import HelloWorld from "../../views/HelloWorld";
+import Top from "../../views/Top";
 import ArticlesByCategory from "../../views/ArticlesByCategory";
 import Articles from "../../views/Articles/Articles";
 import Article from "../../views/Article";
 import SignIn from "../../views/SignIn";
 import ArticleForm from "../../views/ArticleForm/ArticleForm";
-import HelloWorld from "../../views/HelloWorld";
 
 export const router = createBrowserRouter([
   {
@@ -20,6 +21,10 @@ export const router = createBrowserRouter([
       },
       {
         "path": "/",
+        element: <Top />
+      },
+      {
+        "path": "/articles",
         element: <Articles />
       },
       {
